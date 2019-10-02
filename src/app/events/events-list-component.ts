5b81bfa518904c7c75ff13942aa8fa2eff66e7ae
OmGanesh <dreamcodelife@gmail.com>
2019-10-02 10:04:28 -0400

    Pass data from parent component using @input

--- src/app/events/events-list-component.ts
+++ src/app/events/events-list-component.ts
@@ -5,12 +5,12 @@ import {Component} from '@angular/core'
     templateUrl: './events-list-component.html'
 })
 export class EventsListComponent {
-    event = {
+    eventObject = {
         id : 1,
         name: 'Angular Connect',
         date: '9/9/2019',
         time: '10:00AM',
-        price: '499',
+        price: '499.00',
         imageUrl: '/assets/images/angularconnect.jpg',
         location: {
             address:'111 Danforth Ave',
