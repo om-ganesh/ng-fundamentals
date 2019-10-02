3808a8754dfa4f34a9373cb3ed9afc282376fac2
OmGanesh <dreamcodelife@gmail.com>
2019-10-02 09:45:37 -0400

    Use templateUrl for the component templates

--- src/app/events/events-list-component.ts
+++ src/app/events/events-list-component.ts
@@ -2,22 +2,7 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    template: `
-    <div>
-        <h1>Coming events page...</h1>
-        <hr>
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span>Location: {{event.location.city}}, {{event.location.country}}</span>
-            </div>
-        </div>
-    </div>
-    `
+    templateUrl: './events-list-component.html'
 })
 export class EventsListComponent {
     event = {
