4598639c125fb327342bd9c08d01b35988c497c7
OmGanesh <dreamcodelife@gmail.com>
2019-10-02 15:55:02 -0400

    Use OnInit lifecycle hook for calling service for EventList

--- src/app/events/events-list-component.ts
+++ src/app/events/events-list-component.ts
@@ -1,17 +1,17 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import { EventService } from './shared/event.service';
-
 @Component({
     selector: 'events-list',
     templateUrl: './events-list-component.html'
 })
-export class EventsListComponent {
+export class EventsListComponent implements OnInit {
     eventObjects: any[];
-    
-    //TODO: Not good practise to call long running service call in constructor
-    constructor(private eventService: EventService) {
-      this.eventObjects = eventService.getEvents();
+    constructor(private eventService: EventService) {}
+
+    ngOnInit() {
+      this.eventObjects = this.eventService.getEvents();
     }
+
     handleEventClicked(data:any) {
         console.log('Event selected: ' + data.name);
     }
